105f474cb64eda662ee5c008ad07ccdb880bd364
kv89 <kalaskarvishald@gmail.com>
2017-06-28 21:58:35 +0530

    changes ui / nedb

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,15 @@
-var lowdb = require('lowdb');
-const fileAsync = require('lowdb/lib/storages/file-async')
-var db = lowdb('db.json', {storage: fileAsync});
+var nedb = require("nedb"),
+	_db = new nedb({ filename: './user.db', autoload: true });
 
-// db.defaults({users : [{email: 'pchhetri97@gmail.com', password: new Buffer('password').toString('base64')}]}).write();
-// db.get('users').push({email : 'pchhetri97@gmail.com', password: 'cGFzc3dvcmQ='}).write();
+//_db.insert({email: 'pchhetri97@gmail.com', password: new Buffer("password").toString('base64')});
 
 exports.signin = function (req, res) {
-  	console.log('req --- > ',req.body, '  --  ', new Buffer(req.body.email, 'base64').toString());
-  	const user = db.get('users')
-	    .find({email: new Buffer(req.body.email, 'base64').toString()}).value();
-	    console.log(' found user : ' , user);
-	if( user && new Buffer(user.password, 'base64').toString() == new Buffer(req.body.password, 'base64').toString()) {
-		res.send(true);
-	} else {
-		res.send(false);
-	}
+  	_db.findOne({email: new Buffer(req.body.email, 'base64').toString()}, function(err, user) {
+	  	console.log(' found user : ' , user._id);
+		if( user && new Buffer(user.password, 'base64').toString() == new Buffer(req.body.password, 'base64').toString()) {
+			res.send(true);
+		} else {
+			res.send(false);
+		}
+	});
 };
